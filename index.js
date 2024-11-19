6d34b6592f837027ffbf6d52d061c7571ec9ee2f
alan <i39u65f51q@gmail.com>
2024-11-19 17:19:27 +0800

    feat(*): update for ws

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var socket_io_1 = require("socket.io");
 var http_1 = require("http");
-var lodash_1 = require("lodash");
 var port = 3200;
 var server = (0, http_1.createServer)();
 var io = new socket_io_1.Server(server, {
@@ -22,7 +21,6 @@ io.on("connect", function (socket) {
     console.log("new Socket connected:", socket.id);
     // user into room
     socket.on("join", function (name) {
-        console.log("join", name);
         // io.to(roomId).except(socket.id).emit("on-join", res );
         members.push({ name: name, socketId: socket.id });
         io.to(roomId).emit("on-join", { name: name, members: members.map(function (member) { return member.name; }), img: img });
@@ -34,12 +32,11 @@ io.on("connect", function (socket) {
     // user on painting
     socket.on("paint", function (imgUrl) {
         img = imgUrl;
-        var emit = (0, lodash_1.debounce)(function () { return io.to(roomId).except(socket.id).emit("on-paint", img); }, 300);
-        emit();
+        io.to(roomId).except(socket.id).emit("on-paint", img), 500;
     });
     socket.on("mousemove", function (res) {
         // io.to(roomId).except(socket.id).emit("on-mousemove", {name:res.name, x:res.x, y:res.y})
-        io.to(roomId).emit("on-mousemove", { name: res.name, x: res.x, y: res.y });
+        // io.to(roomId).emit("on-mousemove", {name:res.name, x:res.x, y:res.y})
     });
 });
 io.listen(port);
--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
 import { Server } from "socket.io";
 import { createServer } from "http";
-import { debounce } from "lodash";
-
 
 const port = 3200;
 
@@ -24,30 +22,26 @@ io.on("connect", (socket) => {
     console.log("new Socket connected:", socket.id)
 
     // user into room
-    socket.on("join", name  => {          
-        console.log("join", name)    
+    socket.on("join", name  => {                  
         // io.to(roomId).except(socket.id).emit("on-join", res );
         members.push({name:name, socketId:socket.id});
         io.to(roomId).emit("on-join", {name:name, members:members.map((member) => member.name), img:img});        
     })
+    
     socket.on("disconnect", () => {
         console.log('disconnected:', socket.id)
         members= members.filter((member) => member.socketId !== socket.id)
     })
 
     // user on painting
-    socket.on("paint", imgUrl => {        
-        img = imgUrl   
-        const emit = debounce(
-            () => io.to(roomId).except(socket.id).emit("on-paint", img), 300
-        )
-        emit()
-
+    socket.on("paint", imgUrl => {          
+        img = imgUrl         
+        io.to(roomId).except(socket.id).emit("on-paint", img), 500
     })
 
     socket.on("mousemove", (res) => {        
         // io.to(roomId).except(socket.id).emit("on-mousemove", {name:res.name, x:res.x, y:res.y})
-        io.to(roomId).emit("on-mousemove", {name:res.name, x:res.x, y:res.y})
+        // io.to(roomId).emit("on-mousemove", {name:res.name, x:res.x, y:res.y})
     })
 })
 
